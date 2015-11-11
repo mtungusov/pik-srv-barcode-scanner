84ac36eadc8198b28963d7deae653f100746ac5f
TM <tm@macadmin.ru>
2015-11-11 10:17:07 +0300

    msg generator

--- lib/producer.rb
+++ lib/producer.rb
@@ -1,13 +1,21 @@
 module Producer
   def self.produce_random
-    # Generate fields
-    fields = {}
-    _produce(fields)
+    # Generate msg
+    msg = _generate_msg
+    _produce_one(msg)
   end
 
 private
 
-  def self._produce(fields={})
-    puts "Produce message with fields: #{fields}"
+  def self._produce_one(msg={})
+    puts "Produce message: #{msg}"
+  end
+
+  def self._generate_msg
+    {
+      "source": "192.168.254.#{rand 1..4}",
+      "time": Time.now.to_i,
+      "barcode": "000#{(1..9).map {rand 9}.join}"
+    }
   end
 end
